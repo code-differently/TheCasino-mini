9944a33c6c3c4a81482499b33ba2796f33818a1a
CourtneeL <48140227+CourtneeL@users.noreply.github.com>
2020-07-09 15:40:06 -0400

    created casino games

---
+++ src/main/java/com/codedifferently/casino/Player.java
@@ -0,0 +1,33 @@
+package com.codedifferently.casino;
+
+public class Player {
+    private String name;
+
+    //create chip class
+    
+    private int age;
+//After implenting the Card class, add in Card arraylist
+
+public Player(String name, int age){
+    this.name = name;
+    this.age = age;
+}
+
+public String getName(){
+    return this.name;
+}
+
+public int getAge(){
+    if(this.age >= 21){
+    return this.age;
+    }
+    else{
+        return 0;
+    }
+
+}
+
+// Create gamble method after chip class is made
+
+    
+}
