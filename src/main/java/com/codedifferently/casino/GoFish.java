1ad01c652467e46351169244c3b648e21579985b
Paola <camachopaola311@gmail.com>
2020-07-11 22:38:13 -0400

    Added empty game files

---
+++ src/main/java/com/codedifferently/casino/GoFish.java
@@ -0,0 +1,10 @@
+package com.codedifferently.casino;
+
+public class GoFish extends CardGame {
+
+    public GoFish(String gameName, int minPlayers, int maxPlayers) {
+        super(gameName, minPlayers, maxPlayers);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
